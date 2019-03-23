db031974b2b38abef21a0838795cb77d3f6c6249
Daniel Kertesz <daniel@spatof.org>
2019-03-23 11:39:58 +0000

    improve the popup message
    - use different icons for success and error
    - display a preview of the command that was executed
    - use the default notification sound
    - activate iTerm when you click on the alert

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Sends a desktop notification (via `terminal-notifier`) after the command you exe
 running.
 
 We live in frantic days.
+
+<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- zsh-notify.sh
+++ zsh-notify.sh
@@ -4,36 +4,45 @@ zmodload zsh/regex
 # notify if commands was running for more than TIME_THRESHOLD seconds:
 TIME_THRESHOLD=60
 RE_SKIP_COMMANDS="^(ssh|vim|tmux|tig)"
+TERMINAL_BUNDLE="com.googlecode.iterm2"
+SCRIPT_DIR="$(dirname $0:A)"
+
 
 notify() {
-    terminal-notifier -title "⌛ zsh long running job"  -subtitle "$1" -message "$2"
+    # $1 subtitle of the notification (the command that was executed)
+    # $2 the message for the notification
+    # $3 the icon for the notification popup
+    terminal-notifier -title "Long running command" -subtitle "$1" -message "$2" -activate "$TERMINAL_BUNDLE" -sound default \
+                      -appIcon "${SCRIPT_DIR}/$3"
 }
 
 notify-success() {
-    notify "execution finished" \
-           "Command $1 succeded after $2 seconds"
+    notify "$1" "The command succeded after $2 seconds" success.jpg
 }
 
 notify-error() {
-    notify "⚠ execution finished" \
-           "Command $1 failed after $2 seconds: $3"
+    notify "$1" "The command failed after $2 seconds with code: $3" error.png
 }
 
 notify-command-complete() {
-    local now
+    # we must catch $? as soon as possible.
     local last_status=$?
+
+    local now
     local timediff
     now=$(date +%s)
 
-    if [[ $_notify_start_time != "" ]]; then
-        timediff=$(( now - _notify_start_time ))
+    if [[ $_notify_start_time = "" ]]; then
+        return
+    fi
 
-        if (( timediff > TIME_THRESHOLD )); then
-            if [[ $last_status = 0 ]]; then
-                notify-success $_notify_last_command $timediff
-            else
-                notify-error $_notify_last_command $timediff $last_status
-            fi
+    timediff=$(( now - _notify_start_time ))
+
+    if (( timediff > TIME_THRESHOLD )); then
+        if [[ $last_status = 0 ]]; then
+            notify-success $_notify_last_command $timediff
+        else
+            notify-error $_notify_last_command $timediff $last_status
         fi
     fi
 
@@ -42,12 +51,17 @@ notify-command-complete() {
 }
 
 store-command-stats() {
-    if [[ ! $1 -regex-match $RE_SKIP_COMMANDS ]]; then
-        _notify_last_command=${(qqq)1}
-        _notify_start_time=$(date +%s)
+    if [[ $1 -regex-match $RE_SKIP_COMMANDS ]]; then
+        return
     fi
+
+    _notify_last_command="$1"
+    _notify_start_time=$(date +%s)
 }
 
+# For this script to be able to get the exit status of the last executed command ($?)
+# it must be loaded before any other script or function that adds a precmd hook.
+# Only the first precmd hook access the original $?.
 autoload -Uz add-zsh-hook
 add-zsh-hook preexec store-command-stats
 add-zsh-hook precmd notify-command-complete
